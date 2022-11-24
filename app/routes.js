848425e2d2121733bf913a68f03d9c2661fa8f52
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2022-11-24 18:25:31 +0000

    Splitting first and second iterations

--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+// Iteration 1 start ###############################################
+
 // Add your routes here - above the module.exports line
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/edit-record-action', function (req, res) {
+router.post('/iteration-1/edit-record-action', function (req, res) {
 
   // Make a variable and give it the value from 'how-many-balls'
   var action = req.body.action;
 
   if(action == 'save'){
-    res.redirect('/confirmation-saved')
+    res.redirect('/iteration-1/confirmation-saved')
   } else {
-    res.redirect('/confirmation-discarded')
+    res.redirect('/iteration-1/confirmation-discarded')
   }
 
 })
 
-router.post('/edit-set-action', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var action = req.body.action;
-
-  if(action == 'add'){
-    res.redirect('/search')
-  } else if(action == 'edit') {
-    res.redirect('/edit-set-details')
-  } else {
-    res.redirect('/dashboard')
-  }
-
-})
-
-router.post('/dashboard-action', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var action = req.body.action;
-
-  if(action == 'add'){
-    res.redirect('/edit-set-details')
-  } else {
-    res.redirect('/dashboard')
-  }
-
-})
-
-router.post('/login-post', function (req, res) {
+router.post('/iteration-1/login-post', function (req, res) {
 
   // Make a variable and give it the value from 'how-many-balls'
   var username = req.session.data['username']
@@ -53,10 +27,75 @@ router.post('/login-post', function (req, res) {
   // Check whether the variable matches a condition
   if (username == '1234' && password == '1234'){
     // Send user to next page
-    res.redirect('/dashboard')
+    res.redirect('/iteration-1/edit-set')
   } else {
     // Send user to ineligible page
-    res.redirect('/login-invalid')
+    res.redirect('/iteration-1/login-invalid')
+  }
+
+})
+
+
+// Iteration 2 start ##############################################
+
+// Add your routes here - above the module.exports line
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/iteration-2/edit-record-action', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var action = req.body.action;
+
+  if(action == 'save'){
+    res.redirect('/iteration-2/confirmation-saved')
+  } else {
+    res.redirect('/iteration-2/confirmation-discarded')
+  }
+
+})
+
+router.post('/iteration-2/edit-set-action', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var action = req.body.action;
+
+  if(action == 'add'){
+    res.redirect('/iteration-2/search')
+  } else if(action == 'edit') {
+    res.redirect('/iteration-2/edit-set-details')
+  } else {
+    res.redirect('/iteration-2/dashboard')
+  }
+
+})
+
+router.post('/iteration-2/dashboard-action', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var action = req.body.action;
+
+  if(action == 'add'){
+    res.redirect('/iteration-2/edit-set-details')
+  } else {
+    res.redirect('iteration-2/dashboard')
+  }
+
+})
+
+
+
+router.post('/iteration-2/login-post', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var username = req.session.data['username']
+  var password = req.session.data['password']
+
+  // Check whether the variable matches a condition
+  if (username == '1234' && password == '1234'){
+    // Send user to next page
+    res.redirect('/iteration-2/dashboard')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/iteration-2/login-invalid')
   }
 
 })
