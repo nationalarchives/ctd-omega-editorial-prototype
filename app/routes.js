07ffc3f60a23a34a5ea5557d2b678e199a8e6f14
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2022-12-16 09:39:13 +0000

    Modifying add creator for first iteration

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ router.post('/iteration-1/edit-record-action', function (req, res) {
   if(action == 'save'){
     res.redirect('/iteration-1/confirmation-saved')
   } else if(action == 'add-creator'){
-    res.redirect('/iteration-1/edit-record#select-creator')
+    res.redirect('/iteration-1/edit-record-add-creator')
   } else if(action == 'remove-creator'){
     res.redirect('/iteration-1/edit-record#select-creator')
   } else if(action == 'calculate'){
