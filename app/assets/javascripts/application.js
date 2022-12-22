442d3a98b78131cdf217c94b7952ab141db5ae12
Dan Lacey <daniel.lacey@mercator,group>
2022-12-22 16:49:19 +0000

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,16 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+  var l = o.querySelectorAll('[data-module="moj-sortable-table"]');
+t.nodeListForEach(l, (function(e) {
+    new t.SortableTable({
+        table: e
+    })
+})), l = o.querySelectorAll('[data-module="moj-sortable-table"]'), t.nodeListForEach(l, (function(e) {
+    new t.SortableTable({
+        table: e
+    })
+}))
 })
+
+
